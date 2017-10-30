812f47be454e0d74ff9fecdef5f272917e4e90c8
Uwe Stuehler <ustuehler@growit.io>
2017-10-30 18:17:33 +0100

    wip

--- src/plugins/mdc/widgets/action-showsnackbar.js
+++ src/plugins/mdc/widgets/action-showsnackbar.js
@@ -3,7 +3,7 @@ title: $:/plugins/ustuehler/mdc/widgets/action-showsnackbar.js
 type: application/javascript
 module-type: widget
 
-Action widget to show a message and optional action button in the nearest snackbar among siblings of the parent DOM node
+Action widget that shows the first snackbar found in the document with a message and optional action button
 
 \*/
 (function () {
@@ -14,19 +14,19 @@ Action widget to show a message and optional action button in the nearest snackb
 
 var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
-var ShowSnackbarWidget = function(parseTreeNode,options) {
+var ActionShowSnackbarWidget = function(parseTreeNode,options) {
 	this.initialise(parseTreeNode,options);
 };
 
 /*
 Inherit from the base widget class
 */
-ShowSnackbarWidget.prototype = new Widget();
+ActionShowSnackbarWidget.prototype = new Widget();
 
 /*
 Render this widget into the DOM
 */
-ShowSnackbarWidget.prototype.render = function(parent,nextSibling) {
+ActionShowSnackbarWidget.prototype.render = function(parent,nextSibling) {
 	this.computeAttributes();
 	this.execute();
 };
@@ -34,21 +34,22 @@ ShowSnackbarWidget.prototype.render = function(parent,nextSibling) {
 /*
 Compute the internal state of the widget
 */
-ShowSnackbarWidget.prototype.execute = function() {
+ActionShowSnackbarWidget.prototype.execute = function() {
 	this.message = this.getAttribute("message", "");
 	this.actionText = this.getAttribute("actionText", "");
 	this.timeout = parseInt(this.getAttribute("timeout", "2750"));
 
+  // Compute the internal state of child widgets.
   this.makeChildWidgets();
 };
 
 /*
 Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
 */
-ShowSnackbarWidget.prototype.refresh = function(changedTiddlers) {
+ActionShowSnackbarWidget.prototype.refresh = function(changedTiddlers) {
   var changedAttributes = this.computeAttributes();
 
-  if (changedAttributes.message || changedAttributes.actionText) {
+  if (changedAttributes.message || changedAttributes.actionText || changedAttributes.timeout) {
     this.refreshSelf();
     return true;
   }
@@ -59,13 +60,24 @@ ShowSnackbarWidget.prototype.refresh = function(changedTiddlers) {
 /*
  * Invoke the action associated with this widget
  */
-ShowSnackbarWidget.prototype.invokeAction = function(triggeringWidget,event) {
+ActionShowSnackbarWidget.prototype.invokeAction = function(triggeringWidget,event) {
+  var self = this;
   var snackbar = this.document.getElementsByClassName("mdc-snackbar")[0];
+
+  console.log(triggeringWidget);
+  console.log(event);
+  console.log(this.actionText);
+  console.log(this.children);
+  console.log(this);
 
   if (snackbar !== undefined) {
     var data = {
       message: this.message,
       actionText: this.actionText,
+      actionHandler: function() {
+        console.log("Invoking child action widgets");
+        self.invokeActions(triggeringWidget,event);
+      },
       timeout: this.timeout
     };
 
@@ -75,6 +87,13 @@ ShowSnackbarWidget.prototype.invokeAction = function(triggeringWidget,event) {
   return true; // Action was invoked
 }
 
-exports["action-showsnackbar"] = ShowSnackbarWidget;
+/*
+ * Don't allow actions to propagate, because we invoke actions ourself
+ */
+ActionShowSnackbarWidget.prototype.allowActionPropagation = function() {
+  return false;
+};
+
+exports["action-showsnackbar"] = ActionShowSnackbarWidget;
 
 })();
